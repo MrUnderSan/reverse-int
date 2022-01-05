c2ee32921b97eb771da7cd415f6f6e538b6d3f27
MrUnderSan <MrUnderSan@gmail.com>
2022-01-05 22:23:20 +0300

    init: reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse (n) {  
+    let arr = [];
+    for (let i = 0; i < n.toString().length; i++) {
+        arr.push(n.toString()[i]);
+    }
+    reverseArr = arr.reverse();
+    reverseJoin = reverseArr.join('');
+    reverseN = reverseJoin.replace(/\D/g, '');
+    return reverseN;
+};
